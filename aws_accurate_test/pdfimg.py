67df627875292e22ad31307ae994301fb070aa0b
Sahil Gupta <sahil.sg39@gmail.com>
2022-03-31 23:09:49 +0530

    accuracy test 80%

--- aws_accurate_test/pdfimg.py
+++ aws_accurate_test/pdfimg.py
@@ -1,9 +1,9 @@
 import fitz
 import re
 import time
-# from PIL import Image
-# import pytesseract
-# import numpy as np
+from PIL import Image
+import pytesseract
+import numpy as np
 import boto3
 
 
@@ -14,33 +14,33 @@ list1=[] #get data only from list 1
 list2=[] # to get final testdata of dictionery in list2
 
 
-def fromimage(documentName):
+# def fromimage(documentName):
     # Document
-    documentName=documentName
+    # documentName=documentName
 
-    # Read document content
-    with open(documentName, 'rb') as document:
-        imageBytes = bytearray(document.read())
+    # # Read document content
+    # with open(documentName, 'rb') as document:
+    #     imageBytes = bytearray(document.read())
 
-    # Amazon Textract client
-    textract = boto3.client('textract')
+    # # Amazon Textract client
+    # textract = boto3.client('textract')
 
-    # Call Amazon Textract
-    response = textract.detect_document_text(Document={'Bytes': imageBytes})
+    # # Call Amazon Textract
+    # response = textract.detect_document_text(Document={'Bytes': imageBytes})
 
-    #print(response)
-    res=[]
-    # Print detected text
-    a=''
-    for item in response["Blocks"]:
-        if item["BlockType"] == "LINE":
-            # print ('\033[94m' +  item["Text"] + '\033[0m')
-            # new=f'\033[94m' +  item["Text"] + '\033[0m'
-            # print(new)
-            a+=item["Text"] 
-            # res.append(a)
-            # print(a) 
-    return a
+    # #print(response)
+    # res=[]
+    # # Print detected text
+    # a=''
+    # for item in response["Blocks"]:
+    #     if item["BlockType"] == "LINE":
+    #         # print ('\033[94m' +  item["Text"] + '\033[0m')
+    #         # new=f'\033[94m' +  item["Text"] + '\033[0m'
+    #         # print(new)
+    #         a+=item["Text"] 
+    #         # res.append(a)
+    #         # print(a) 
+    # return a
 
 
 def pdftotext(file):
@@ -84,17 +84,20 @@ def imgtotxt(file):
             
             
             filename = f'images/image_Page{pageNumber}_{imgNumber}.png'
-            # img1 = np.array(Image.open(filename))
-            # text = pytesseract.image_to_string(img1)
-            documentName=filename
-            text=fromimage(documentName)
+            
+            # documentName=filename
+            # text=fromimage(documentName).
+            # pytesrect
+            img1 = np.array(Image.open(filename))
+            text = pytesseract.image_to_string(img1)
+            # end pyt
             text=text.lower()
             alltext2+=text
             
     alltext+=alltext2
     print(alltext)
     get_date_name=get_name_date(alltext)
-    print(get_date_name)
+    # print(get_date_name)
     
     test_names=gettest(alltext)
     if not list1 and not list2:
@@ -163,26 +166,32 @@ def gettest(text):
 
     # get test value
     
-    test_name=[['haemoglobin (hb)','rbc count.','platelet count','meditest full body checkup panel','tlc (total leucocyte count)'],
-    ['liver function test','lft','bilirubin, total','sgot','sgpt']]
+    test_name=[['mch','rbc count','platelet count','meditest full body checkup panel','tlc (total leucocyte count)'],
+    ['bilirubin, total','sgot','sgpt'],['blood urea'],['hba1c'],['vitamin b12']]
     
-    dictdata={}
+    
     def test_value(datas):
         def sub_test_value(datas):
-            
+    
             # a=re.compile(fr'{datas} ^\d*[.,]?\d*$ ')
             
             
-            # a=re.compile(rf'{datas}\s*\n\d*[0-9]*[.,]?[0-9]*')
-            a=re.compile(rf'{datas}\s*\n\s*[a-zA-Z]?\d*[0-9]*[.,]?[0-9]*')
+            
+            # t_value1=re.compile(rf'{(datas)}\s*\n\s*[a-zA-Z]?\d*[0-9]*[.]?[0-9]*')
+            
+            # t_value2=re.compile(rf'{datas}\s*\n\d*[0-9]*[.,]?[0-9]*')
+            print(datas)
 
-
-            a=a.search(text)
-            print('print datas value',a,'end datas value')
-            # a=a.search(text)
-            if a!=None:
-                # print(a.group())
-                return a.group() 
+            # t_value1=re.compile(rf'(?={datas})(.*?|\n)*(\d*[0-9]*[.]?[0-9])')
+            t_value1=re.compile(rf'({datas})(.*?|\n)*(\d*[0-9]*[.]?[0-9])')
+            t_value1=t_value1.search(text)
+            
+           
+            if t_value1!=None:
+                print(t_value1.group())
+                return t_value1.group() 
+                
+           
         subtestvalue=list(map(sub_test_value,datas))
         return subtestvalue
 
@@ -199,10 +208,10 @@ if __name__ == "__main__":
     # file=fitz.open('share_preview2.pdf')
     # file=fitz.open('c676422960523ad28beb131038335370.pdf')
     
-    file=fitz.open('lalita.pdf')
+    # file=fitz.open('lalita.pdf')
     # file=fitz.open('prem.pdf')
     # file=fitz.open('imagetest.pdf')
-    # file=fitz.open('jyoti.pdf')
+    file=fitz.open('jyoti.pdf')
     # file wil go in pdftotext
     p2t_value=pdftotext(file)# get value from pdftotext.either it will have text or empty value
     print('pdftotext',p2t_value)
